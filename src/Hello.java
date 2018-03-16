2679a748de8e7f5b6c5401b3590e88215cc1a448
Minase Mengistu <meketeminase4@gmail.com>
2018-03-16 19:19:59 +0100

    New Display function

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,13 @@
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println(printMessage());
+		
+	}
+	
+	public static String printMessage() {
+		
+		return("Hello world from a method!!!");
 	}
 
 }
