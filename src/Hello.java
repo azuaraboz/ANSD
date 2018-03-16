fb3f62f01277767f5f2a3166b216eeb3abdd18bd
Minase Mengistu <meketeminase4@gmail.com>
2018-03-16 21:56:28 +0100

    Orient db connected from client

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,30 @@
-
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.sql.executor.OResult;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println(printMessage());
+		//System.out.println(printMessage());
+		
+		OrientDB orient = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig());
+
+		ODatabaseSession db = orient.open("Movies", "root", "root123456");
+		String query = "select from movie";
+		OResultSet rs = db.query(query);
+		
+		while(rs.hasNext())
+		{
+			OResult item = rs.next();
+			 System.out.println(item.getProperty("Title").toString());
+		}
+		
+		
+		
+		db.close();
+
+	    orient.close();
 		
 	}
 	
